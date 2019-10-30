413dd978c380c8c2acbacb4db3e6a2faf229b1d2
Nikolay <54440860+batoxa@users.noreply.github.com>
2019-10-31 02:12:46 +0300

    ферма готова

--- farm.js
+++ farm.js
@@ -1,4 +1,19 @@
+/*  ... ЗАДАЧА ...
+Ферма.На ферме есть куры.Кур жрут лисы.
+Фермер решил потравить лис.Отрава для кур безвредна, для лис смертельна.
+Куры бывают в клетках.Если курица в клетке, то лиса не может съесть курицу,
+но если лиса в клетке с курицей, то тогда может.
+Надо написать пошаговый симулятор жизни на ферме.
+Лисы - F, куры - C, отрава - Х, клетка - [].
+Ферма выглядит, например, так: CXC[CC]CC[CCXCF]CC[CFC]FCC.
+Голодная лиса съедает за один ход максимальное количество куриц,
+пока не дойдет до отравы.Одна отрава может убить любое количество лис.
+Лиса может перепрыгивать клетки, но не залезает внутрь и не вылезает наружу.
+Длинна строки не меняется, на место сожраной куры или отравившейся лисы ставим "."
+*/
+
 const world = "CXC[CC]CC[CCXCF]CC[CFC]FCC";
+let result = "";
 const farm = [];
 let cell = false;
 
@@ -12,12 +27,76 @@ for (let i = 0; i < world.length; i++) {
     } else {
         farm[i].cell = true;
     }
-    if ((world[i] === "[") && (cell === false)) {
+    if (world[i] === "[") {
         farm[i].cell = true;
         cell = true;
-    } else if ((world[i - 1] === "]") && (cell === true)) {
+    } else if (world[i - 1] === "]") {
         farm[i].cell = false;
         cell = false;
     }
 }
-console.log(farm);
+
+function hunt(foxIndex, cellAffiliation) {
+    if (cellAffiliation === true) {
+        for (let x = (foxIndex - 1);
+            (farm[x].type != "[") && (farm[foxIndex].type != "Y"); x--) {
+            switch (farm[x].type) {
+                case "C":
+                    farm[x].type = ".";
+                    break;
+                case "X":
+                    farm[foxIndex].type = "Y";
+            }
+        }
+        for (let x = (foxIndex + 1);
+            (farm[x].type != "]") && (farm[foxIndex].type != "."); x++) {
+            switch (farm[x].type) {
+                case "C":
+                    farm[x].type = ".";
+                    break;
+                case "X":
+                    farm[foxIndex].type = ".";
+            }
+        }
+        if (farm[foxIndex].type === "Y") { farm[foxIndex].type = "."; }
+    } else {
+        for (let x = (foxIndex - 1);
+            (x > 0) && (farm[foxIndex].type != "Y"); x--) {
+            if (farm[x].cell === false) {
+                switch (farm[x].type) {
+                    case "C":
+                        farm[x].type = ".";
+                        break;
+                    case "X":
+                        farm[foxIndex].type = "Y";
+                }
+            }
+        }
+        for (let x = (foxIndex + 1);
+            (x < farm.length) && (farm[foxIndex].type != "."); x++) {
+            if (farm[x].cell === false) {
+                switch (farm[x].type) {
+                    case "C":
+                        farm[x].type = ".";
+                        break;
+                    case "X":
+                        farm[foxIndex].type = ".";
+                }
+            }
+        }
+        if (farm[foxIndex].type === "Y") { farm[foxIndex].type = "."; }
+    }
+}
+
+for (let i = 0; i < farm.length; i++) {
+    if (farm[i].type === "F") {
+        hunt(i, farm[i].cell);
+    }
+}
+
+for (let i = 0; i < farm.length; i++) {
+    result += farm[i].type;
+}
+console.log(world);
+console.log(result);
+console.log();
