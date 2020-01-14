fb790e29e03bf8947d50a9d69827f614aa29ddde
batoxa <nbatorin@gmail.com>
2020-01-14 17:54:32 +0300

    new

--- codewars/sum2.js
+++ codewars/sum2.js
@@ -1,7 +1,20 @@
 // https://www.codewars.com/kata/541c8630095125aba6000c00
 
-function digital_root(n) {
-    let x = String(n).split("").reduce((sum, current) => sum + +current, 0);
-    return x > 9 ? digital_root(x) : x;
+// function digital_root(n) {
+//     let x = String(n).split("").reduce((sum, current) => sum + +current, 0);
+//     return x > 9 ? digital_root(x) : x;
+// };
+// console.log(digital_root(1633));
+
+
+
+function solution(str) {
+    let x = str.split("");
+    x.length % 2 ? x.push('_') : 0;
+    console.log(x);
 };
-console.log(digital_root(1633));
+
+
+
+solution('abcw') // should return ['ab', 'c_']
+    // solution('abcdef') // should return ['ab', 'cd', 'ef']
