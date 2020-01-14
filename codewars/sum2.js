6fcc005e90adf593414442b282fc1639caa7985d
batoxa <nbatorin@gmail.com>
2020-01-14 17:25:00 +0300

    edit sum2

--- codewars/sum2.js
+++ codewars/sum2.js
@@ -3,10 +3,7 @@
 function digital_root(n) {
     let numbers = String(n).split("");
     let x = numbers.reduce((sum, current) => sum + +current, 0);
-    if (x > 9) {
-        return digital_root(x);
-    } else {
-        return x;
-    };
+    if (x > 9) return digital_root(x);
+    return x;
 };
-console.log(digital_root(1633));
+console.log(digital_root(55));
