26783002354d45958587410aaf691f6306482d1d
batoxa <nbatorin@gmail.com>
2020-01-14 17:29:31 +0300

    cut sum2

--- codewars/sum2.js
+++ codewars/sum2.js
@@ -1,9 +1,7 @@
 // https://www.codewars.com/kata/541c8630095125aba6000c00
 
 function digital_root(n) {
-    let numbers = String(n).split("");
-    let x = numbers.reduce((sum, current) => sum + +current, 0);
-    if (x > 9) return digital_root(x);
-    return x;
+    let x = String(n).split("").reduce((sum, current) => sum + +current, 0);
+    return x > 9 ? digital_root(x) : x;
 };
-console.log(digital_root(55));
+console.log(digital_root(1633));
