5c37c0f28c3d1af3b76f4c38b730267766b3ad7f
batoxa <nbatorin@gmail.com>
2020-01-16 17:51:34 +0300

    big

--- codewars/#bignum.js
+++ codewars/#bignum.js
@@ -1,12 +1,20 @@
 // https://www.codewars.com/kata/55983863da40caa2c900004e/train/javascript
 function nextBigger(n) {
-    //your code here
+    const num = a.split("").map(function(name) {
+        return Number(name);
+    });
+
+
+
+
+    return num;
 }
 
+console.log(nextBigger(12));
 
 
-Test.assertEquals(nextBigger(12), 21)
-Test.assertEquals(nextBigger(513), 531)
-Test.assertEquals(nextBigger(2017), 2071)
-Test.assertEquals(nextBigger(414), 441)
-Test.assertEquals(nextBigger(144), 414)
+// Test.assertEquals(nextBigger(12), 21)
+// Test.assertEquals(nextBigger(513), 531)
+// Test.assertEquals(nextBigger(2017), 2071)
+// Test.assertEquals(nextBigger(414), 441)
+// Test.assertEquals(nextBigger(144), 414)
