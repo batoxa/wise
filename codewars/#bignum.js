8507341be04776efaab8f929dcabccb311d03fd7
batoxa <nbatorin@gmail.com>
2020-01-16 22:21:26 +0300

    bignum done

--- codewars/#bignum.js
+++ codewars/#bignum.js
@@ -1,20 +1,27 @@
 // https://www.codewars.com/kata/55983863da40caa2c900004e/train/javascript
+
 function nextBigger(n) {
-    const num = a.split("").map(function(name) {
+    const num = String(n).split('').map(function(name) {
         return Number(name);
     });
+    console.log(num);
+    for (i = num.length - 1; i > -1; i--) {
+        if (num[i] > num[i - 1]) {
+            const num2 = num.splice(i, num.length - i).sort();
+            const x = num[i - 1];
+            for (j = 0; j < num2.length; j++) {
+                if (num2[j] > x) {
+                    num[num.length - 1] = num2[j];
+                    num2[j] = x;
+                    break;
+                };
+            };
+            num.push(...num2);
+            console.log(num);
 
-
-
-
-    return num;
-}
-
-console.log(nextBigger(12));
-
-
-// Test.assertEquals(nextBigger(12), 21)
-// Test.assertEquals(nextBigger(513), 531)
-// Test.assertEquals(nextBigger(2017), 2071)
-// Test.assertEquals(nextBigger(414), 441)
-// Test.assertEquals(nextBigger(144), 414)
+            return Number(num.join(''));
+        };
+    };
+    return -1;
+};
+nextBigger(12);
