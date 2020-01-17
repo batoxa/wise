67b0ba0a2ac4680a298ae7a9350788da1dbf6df2
batoxa <nbatorin@gmail.com>
2020-01-18 00:31:34 +0300

    bowling edit

--- test.js
+++ test.js
@@ -5,10 +5,29 @@ function bowlingScore(frames) {
     const num = num2.filter(el => el !== ' ');
     let sum = 0;
     for (i = 0; i < num.length; i++) {
+
         if (num[i] === 'X') {
-            sum += 10;
-            sum += num[i + 1] === 'X' ? 10 : +num[i + 1];
-            sum += num[i + 2] === 'X' ? 10 : +num[i + 2];
+            // sum += 10;
+            sum += i > num.length - 4 ? 0 : 10;
+
+            if (num[i + 1]) {
+                if (num[i + 1] === 'X') {
+                    sum += 10;
+                } else {
+                    sum += +num[i + 1];
+                };
+            };
+            if (num[i + 2]) {
+                if (num[i + 2] === 'X') {
+                    sum += 10;
+                } else if (num[i + 2] === '/') {
+                    sum += 10 - +num[i + 1];
+
+                } else {
+                    sum += +num[i + 2];
+                };
+            };
+
         } else if (num[i] === '/') {
             sum += 10 - +num[i - 1];
             if (num[i + 1]) {
@@ -17,8 +36,9 @@ function bowlingScore(frames) {
                 } else {
                     sum += +num[i + 1];
                 };
-                console.log('!!!');
+
             };
+
         } else {
             sum += +num[i];
         };
@@ -27,10 +47,14 @@ function bowlingScore(frames) {
 
 
     console.log(sum);
-    return console.log(num);
+    console.log(num);
+    return sum;
 };
 
-bowlingScore('0123X5/');
+// bowlingScore('11X1');
+// bowlingScore('01235/');
+
+bowlingScore('34 2/ 90 61 08 X X 34 80 XX1');
 // bowlingScore('1/ 11 X1 11 1/ 11 11 11 11 11');
 // bowlingScore('X X 9/ 80 X X 90 8/ 7/ 44');
 
